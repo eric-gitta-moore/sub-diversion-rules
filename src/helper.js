ff6784f42bd946450782318d73d6704daa2a858e
Eric Moore <EricGittaMoore@duck.com>
2024-03-01 12:59:40 +0800

    add GoogleOneVPN

--- src/helper.js
+++ src/helper.js
@@ -74,6 +74,7 @@ function useGenGroupHelper() {
     return [
       { name: "Abroad SSH", ...useProxiesArray() },
       { name: "Google", ...useProxiesArray() },
+      { name: "GoogleOneVPN", ...useProxiesArray() },
       { name: "AWS", ...useProxiesArray() },
       { name: "Microsoft", ...useProxiesArray() },
       { name: "TikTok", ...useProxiesArray() },
--- src/rule-provider.js
+++ src/rule-provider.js
@@ -1,5 +1,6 @@
 export const ruleProvider = {
     'LocalAreaNetwork': `https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/LocalAreaNetwork.yaml`,
     'AboardNetwork': `https://raw.githubusercontent.com/james-curtis/sub-diversion-rules/main/rule/james-curtis/Clash/Providers/AboardNetwork.yaml`,
+    'GoogleOneVPN': `https://raw.githubusercontent.com/james-curtis/sub-diversion-rules/main/rule/james-curtis/Clash/Providers/GoogleOneVPN.yaml`,
     'TikTok': `https://github.com/blackmatrix7/ios_rule_script/raw/master/rule/Clash/TikTok/TikTok.yaml`,
 }
