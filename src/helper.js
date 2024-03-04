09202f6afa14ba313e75bb5ad2116c6e97aa2d5f
Eric Moore <EricGittaMoore@duck.com>
2024-03-05 00:20:04 +0800

    fix: update

--- src/helper.js
+++ src/helper.js
@@ -102,7 +102,7 @@ function useGenGroupHelper() {
     function genAriaGroup(name, filter = null) {
       return {
         name,
-        "include-all": true,
+        "include-all-proxies": true,
         filter: !filter ? `(?i)(${name})` : filter,
       };
     }
