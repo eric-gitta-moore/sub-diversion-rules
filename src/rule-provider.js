ed64996b4e68b1e93a91167c57866543484c2b54
Eric Moore <EricGittaMoore@duck.com>
2024-03-07 18:34:40 +0800

    fix: update

--- src/rule-provider.js
+++ src/rule-provider.js
@@ -1,7 +1,7 @@
 export const ruleProvider = {
-    'LocalAreaNetwork': `https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/LocalAreaNetwork.yaml`,
-    'AboardNetwork': `https://raw.githubusercontent.com/james-curtis/sub-diversion-rules/main/rule/james-curtis/Clash/Providers/AboardNetwork.yaml`,
-    'GoogleOneVPNCheckPoint': `https://raw.githubusercontent.com/james-curtis/sub-diversion-rules/main/rule/james-curtis/Clash/Providers/GoogleOneVPNCheckPoint.yaml`,
-    'GoogleOneVPNTun': `https://raw.githubusercontent.com/james-curtis/sub-diversion-rules/main/rule/james-curtis/Clash/Providers/GoogleOneVPNTun.yaml`,
-    'TikTok': `https://github.com/blackmatrix7/ios_rule_script/raw/master/rule/Clash/TikTok/TikTok.yaml`,
+    'LocalAreaNetwork': `https://mirror.ghproxy.com/https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/LocalAreaNetwork.yaml`,
+    'AboardNetwork': `https://mirror.ghproxy.com/https://raw.githubusercontent.com/james-curtis/sub-diversion-rules/main/rule/james-curtis/Clash/Providers/AboardNetwork.yaml`,
+    'GoogleOneVPNCheckPoint': `https://mirror.ghproxy.com/https://raw.githubusercontent.com/james-curtis/sub-diversion-rules/main/rule/james-curtis/Clash/Providers/GoogleOneVPNCheckPoint.yaml`,
+    'GoogleOneVPNTun': `https://mirror.ghproxy.com/https://raw.githubusercontent.com/james-curtis/sub-diversion-rules/main/rule/james-curtis/Clash/Providers/GoogleOneVPNTun.yaml`,
+    'TikTok': `https://mirror.ghproxy.com/https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/TikTok/TikTok.yaml`,
 }
