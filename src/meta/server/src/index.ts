b985139932456008918795381d6f1c6581008274
Eric Moore <EricGittaMoore@duck.com>
2024-09-20 21:22:37 +0800

    update

--- src/meta/server/src/index.ts
+++ src/meta/server/src/index.ts
@@ -2,6 +2,9 @@ import fs from "node:fs";
 import Koa from "koa";
 import path from "node:path";
 
+const defaultTplUrl = 'https://github.com/eric-gitta-moore/sub-diversion-rules/raw/main/src/meta/server/assets/base.yaml'
+const tplUrl = process.env.TPL_URL || defaultTplUrl
+
 const test = fs.readFileSync(
   path.resolve(process.cwd(), "./assets/base.yaml"),
   "utf-8",
@@ -10,8 +13,14 @@ const test = fs.readFileSync(
 const app = new Koa();
 
 app.use(async (ctx) => {
+  let tpl = test
+  try {
+    tpl = await fetch(tplUrl).then(e => e.text())
+  } catch (e) {
+    console.warn('获取远程配置失败', e)
+  }
   const url: string = (ctx.query?.url as string) || "";
-  ctx.body = test.replace(`http://test.com`, decodeURIComponent(url));
+  ctx.body = tpl.replace(`http://test.com`, decodeURIComponent(url));
 });
 
 const server = app.listen(3000);
